d676feec0263eeaa1ff6035b79d5fcb30984ea12
ac1dgoddess <ac1dg0dd3ss@gmail.com>
2020-01-07 21:03:48 -0600

    pixel updates

--- src/components/HeaderImage/Wrapper.js
+++ src/components/HeaderImage/Wrapper.js
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
   border: 5px solid #6d1be0;
   border-radius: 50%;
-  height: 150px;
-  margin: 0 auto 15px auto;
+  height: 142px;
+  margin: 0 auto 10px auto;
   overflow: hidden;
   width: 150px;
 
